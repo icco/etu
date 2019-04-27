dd341cb86849368400061eaa1080758c159406c4
Nat Welch <nat@natwelch.com>
2019-04-27 19:49:05 +0000

    lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import withApolloClient from '../lib/with-apollo-client'
-import { ApolloProvider } from 'react-apollo'
+import App, { Container } from "next/app";
+import React from "react";
+import withApolloClient from "../lib/with-apollo-client";
+import { ApolloProvider } from "react-apollo";
 
 class Etu extends App {
-  render () {
-    const { Component, pageProps, apolloClient } = this.props
+  render() {
+    const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
       </Container>
-    )
+    );
   }
 }
 
-export default withApolloClient(Etu)
+export default withApolloClient(Etu);
