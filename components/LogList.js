6c828d57953594f9b682afffb0411a1604d2d863
Nat Welch <nat@natwelch.com>
2019-04-29 23:24:00 +0000

    lint

--- components/LogList.js
+++ components/LogList.js
@@ -9,28 +9,28 @@ const PER_PAGE = 20;
 
 function LogList({ data: { loading, error, logs } }) {
   if (error) return <ErrorMessage message="Error loading User's Logs." />;
-  if (loading) { return <Loading key={0} />; }
-    return (
-      <section className="mw8 center">
-          <ul className="list pl0" key="ul">
-            {logs.map(log=> (
-              <li className="mb5 ml4 mr3" key={"log-list-" + log.id}>
-                <div className="f6 db pb1 gray">
-                  <span className="dbi mr3">{log.code}</span>
-                  <span className="dbi mr3">{log.project}</span>
-                  <span className="dbi mr3">{log.datetime}</span>
-                <Link as={`/log/${log.id}`} href={`/log?id=${log.id}`}>
-                  <a className="dbi ">{log.id}</a>
-                </Link>
-                </div>
-              <div>
-              {log.description}
-              </div>
-              </li>
-            ))}
-          </ul>
-      </section>
-    );
+  if (loading) {
+    return <Loading key={0} />;
+  }
+  return (
+    <section className="mw8 center">
+      <ul className="list pl0" key="ul">
+        {logs.map(log => (
+          <li className="mb5 ml4 mr3" key={"log-list-" + log.id}>
+            <div className="f6 db pb1 gray">
+              <span className="dbi mr3">{log.code}</span>
+              <span className="dbi mr3">{log.project}</span>
+              <span className="dbi mr3">{log.datetime}</span>
+              <Link as={`/log/${log.id}`} href={`/log?id=${log.id}`}>
+                <a className="dbi ">{log.id}</a>
+              </Link>
+            </div>
+            <div>{log.description}</div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export const userLogs = gql`
@@ -51,5 +51,5 @@ export default graphql(userLogs, {
       offset: 0,
       perpage: PER_PAGE,
     },
-  }
+  },
 })(LogList);
