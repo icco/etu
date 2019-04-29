3446aa7bfd0404fd082c2e1296cebaddb938e8b5
Nat Welch <nat@natwelch.com>
2019-04-29 23:31:05 +0000

    links

--- components/LogList.js
+++ components/LogList.js
@@ -18,11 +18,13 @@ function LogList({ data: { loading, error, logs } }) {
         {logs.map(log => (
           <li className="mb5 ml4 mr3" key={"log-list-" + log.id}>
             <div className="f6 db pb1 gray">
-              <span className="dbi mr3">{log.code}</span>
-              <span className="dbi mr3">{log.project}</span>
-              <span className="dbi mr3">{log.datetime}</span>
+              <span className="db dbi-ns mr3">{log.code}</span>
+              <Link as={`/wiki/${log.project}`} href={`/wiki?id=${log.project}`}>
+                <a className="db dbi-ns mr3">{log.project}</a>
+              </Link>
+              <span className="db dbi-ns mr3">{log.datetime}</span>
               <Link as={`/log/${log.id}`} href={`/log?id=${log.id}`}>
-                <a className="dbi ">{log.id}</a>
+                <a className="db dbi-ns mr3">{log.id}</a>
               </Link>
             </div>
             <div>{log.description}</div>
