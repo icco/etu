f2caab22b3dc2823ad49323e1ef0e62e773613cd
Nat Welch <nat@natwelch.com>
2019-05-13 00:22:49 +0000

    duration

--- components/Submit.js
+++ components/Submit.js
@@ -93,9 +93,10 @@ class Submit extends React.Component {
                     onChange={this.handleBasicChange}
                   />
                   <small id="code-desc" className="f6 black-60 db mb2">
-                    Helper text for the form control.
+                    Three numbers.
                   </small>
                 </div>
+
                 <div className="measure mv2">
                   <label htmlFor="content" className="f6 b db mb2">
                     Log Entry
@@ -108,9 +109,10 @@ class Submit extends React.Component {
                     onChange={this.handleBasicChange}
                   />
                   <small id="content-desc" className="f6 black-60 db mb2">
-                    Helper text for the form control.
+                    What's your update?
                   </small>
                 </div>
+
                 <div className="measure mv2">
                   <label htmlFor="project" className="f6 b db mb2">
                     Project
@@ -123,9 +125,26 @@ class Submit extends React.Component {
                     aria-describedby="project-desc"
                   />
                   <small id="project-desc" className="f6 black-60 db mb2">
-                    Helper text for the form control.
+                    What project is this?
                   </small>
                 </div>
+
+                <div className="measure mv2">
+                  <label htmlFor="duration" className="f6 b db mb2">
+                    Duration
+                  </label>
+                  <input
+                    id="duration"
+                    className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                    type="text"
+                    onChange={this.handleBasicChange}
+                    aria-describedby="duration-desc"
+                  />
+                  <small id="duration-desc" className="f6 black-60 db mb2">
+                    How long were you working on this update?
+                  </small>
+                </div>
+
                 <input
                   className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                   type="submit"
