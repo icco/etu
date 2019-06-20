a9a37f870aa362ceeca9ef53b3a242b1b543847a
Nat Welch <nat@natwelch.com>
2019-06-20 19:19:31 +0000

    remove fathom

--- components/App.js
+++ components/App.js
@@ -1,9 +1,4 @@
 import React from "react";
 import Router from "next/router";
-import * as fathom from "../lib/fathom";
-
-Router.onRouteChangeComplete = url => {
-  fathom.pageview(url);
-};
 
 export default ({ children }) => <main>{children}</main>;
