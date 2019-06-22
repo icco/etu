33775f150eea41d9eee7bb5a4d3cae9357a8f6fb
Nat Welch <nat@natwelch.com>
2019-06-22 15:21:22 +0000

    lint

--- components/Main.js
+++ components/Main.js
@@ -3,23 +3,22 @@ import React from "react";
 
 class Main extends React.Component {
   render() {
-  let content = (
-    <>
-    <h1 class="f-headline mw7 center">Please sign-in to see logs.</h1>
-    </>
-  );
-
-  if (this.props.loggedInUser) {
-    content = (
+    let content = (
       <>
-        <Submit />
-        <LogList />
+        <h1 class="f-headline mw7 center">Please sign-in to see logs.</h1>
       </>
     );
-  }
 
+    if (this.props.loggedInUser) {
+      content = (
+        <>
+          <Submit />
+          <LogList />
+        </>
+      );
+    }
 
-    return content
+    return content;
   }
 }
 
