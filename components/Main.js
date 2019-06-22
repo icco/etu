1a2ac3dec4d49e688f759bc5226c745646cfe0d2
Nat Welch <nat@natwelch.com>
2019-06-22 15:21:04 +0000

    Move Index to main

---
+++ components/Main.js
@@ -0,0 +1,26 @@
+import { withRouter } from "next/router";
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+  let content = (
+    <>
+    <h1 class="f-headline mw7 center">Please sign-in to see logs.</h1>
+    </>
+  );
+
+  if (this.props.loggedInUser) {
+    content = (
+      <>
+        <Submit />
+        <LogList />
+      </>
+    );
+  }
+
+
+    return content
+  }
+}
+
+export default withRouter(Main);
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,19 @@ import Head from "next/head";
 
 import App from "../components/App";
 import Header from "../components/Header";
-import Submit from "../components/Submit";
 import LogList from "../components/LogList";
+import Main from "../components/Main";
+import Submit from "../components/Submit";
 import { checkLoggedIn } from "../lib/auth";
 
 const Index = props => {
-  let content = (
-    <h1 class="f-headline mw7 center">Please sign-in to see logs.</h1>
-  );
-
-  if (props.loggedInUser) {
-    content = (
-      <>
-        <Submit />
-        <LogList />
-      </>
-    );
-  }
-
   return (
     <App>
       <Head>
         <title>Etu Time Tracking</title>
       </Head>
       <Header loggedInUser={props.loggedInUser} noLogo />
-      {content}
+      <Main loggedInUser={props.loggedInUser} />
     </App>
   );
 };
