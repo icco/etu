31ad4e45b9c08a51b2f44a3f018a470b352d936d
Nat Welch <nat@natwelch.com>
2020-01-26 03:59:44 +0000

    more

--- components/LogList.js
+++ components/LogList.js
@@ -1,27 +1,11 @@
-import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 import Link from "next/link";
 import { ErrorMessage, Loading } from "@icco/react-common";
+import { useQuery } from "@apollo/react-hooks";
 
 import Log from "./Log";
 
 const PER_PAGE = 20;
-
-function LogList({ data: { loading, error, logs } }) {
-  if (error) return <ErrorMessage message="Error loading User's Logs." />;
-  if (loading) {
-    return <Loading key={0} />;
-  }
-  return (
-    <section className="mw8">
-      <ul className="list pl0" key="ul">
-        {logs.map(log => (
-          <Log key={log.id} id={log.id} data={{ log }} />
-        ))}
-      </ul>
-    </section>
-  );
-}
 
 export const userLogs = gql`
   query logs {
@@ -31,11 +15,24 @@ export const userLogs = gql`
   }
 `;
 
-export default graphql(userLogs, {
-  options: {
-    variables: {
+export default function LogList() {
+  const { loading, error, data } = useQuery(userLogs, {
+    variables: { 
       offset: 0,
       perpage: PER_PAGE,
     },
-  },
-})(LogList);
+  });
+  if (error) return <ErrorMessage message="Error loading User's Logs." />;
+  if (loading) {
+    return <Loading key={0} />;
+  }
+  return (
+    <section className="mw8">
+      <ul className="list pl0" key="ul">
+        {logs.map(log => (
+          <Log key={log.id} id={log.id} />
+        ))}
+      </ul>
+    </section>
+  );
+}
