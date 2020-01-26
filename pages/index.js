4afee02ca2cc584dc1155a2fdb94dbf194568073
Nat Welch <nat@natwelch.com>
2020-01-26 04:01:27 +0000

    clean up

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Head from "next/head";
 import { withAuth, withLoginRequired } from "use-auth0-hooks";
 
 import App from "../components/App";
-import Main from "../components/Main";
 import Header from "../components/Header";
 import NotAuthorized from "../components/NotAuthorized";
 import { withApollo } from "../lib/apollo";
 import { useLoggedIn } from "../lib/auth";
+import LogList from "../components/LogList";
+import Submit from "../components/Submit";
 
 const Page = ({ auth }) => {
   const { loggedInUser } = useLoggedIn();
@@ -19,8 +20,9 @@ const Page = ({ auth }) => {
       <Head>
         <title>Etu Time Tracking</title>
       </Head>
-      <Header loggedInUser={this.state.loggedInUser} noLogo />
-      <Main loggedInUser={this.state.loggedInUser} />
+      <Header noLogo />
+          <Submit />
+          <LogList />
     </App>
   );
 };
