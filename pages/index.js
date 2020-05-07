aaa7d358407f9438d2a0606d010296eac45349aa
Nat Welch <nat@natwelch.com>
2020-05-07 03:27:39 +0000

    progress

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Head from "next/head";
 import { withAuth, withLoginRequired } from "use-auth0-hooks";
+import { ErrorMessage, Loading } from "@icco/react-common";
 
 import { withApollo } from "../lib/apollo";
-import { AuthOptions } from "../lib/auth";
+import { useLoggedIn } from "../lib/auth";
 
 import App from "../components/App";
-import Main from "../components/Main";
 import Header from "../components/Header";
 import NotAuthorized from "../components/NotAuthorized";
+import Submit from "../components/Submit";
+import LogList from "../components/LogList";
 
 const Page = ({ auth }) => {
+  const { loggedInUser, loading, error } = useLoggedIn();
+
+  if (error) {
+    return <ErrorMessage message="Error loading User's Logs." />;
+  }
+
+  if (loading) {
+    return <Loading key={0} />;
+  }
+
+  if (!loggedInUser) {
+    return <ErrorMessage message="User not logged in." />;
+  }
+
   return (
     <App>
       <Head>
@@ -18,9 +34,10 @@ const Page = ({ auth }) => {
 
       <Header noLogo />
 
-      <Main />
+      <Submit loggedInUser={loggedInUser} />
+      <LogList loggedInUser={loggedInUser} />
     </App>
   );
 };
 
-export default withLoginRequired(withAuth(withApollo(Page), AuthOptions));
+export default withLoginRequired(withAuth(withApollo(Page)));
