a5ea149d0b5f4227f492fdcf2733922ce547fe77
Nat Welch <nat@natwelch.com>
2020-05-07 03:18:00 +0000

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-// https://github.com/zeit/next-plugins/tree/master/packages/next-css
-const withCSS = require("@zeit/next-css");
 const port = process.env.PORT || 8080;
-module.exports = withCSS({
+module.exports = {
   env: {
     GRAPHQL_ORIGIN: process.env.GRAPHQL_ORIGIN,
     AUTH0_CLIENT_ID: "MwFD0COlI4F4AWvOZThe1psOIletecnL",
@@ -9,4 +7,4 @@ module.exports = withCSS({
     DOMAIN: process.env.DOMAIN || `http://localhost:${port}`,
     PORT: port,
   },
-});
+};
