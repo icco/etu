dd9f501c5d58070aa490cde0d3a447e0e9eedfac
Nat Welch <nat@natwelch.com>
2020-12-31 02:52:40 +0000

    sure

---
+++ download.go
@@ -0,0 +1,66 @@
+package etu
+
+import (
+	"context"
+	"fmt"
+
+	gql "github.com/icco/graphql"
+	"github.com/machinebox/graphql"
+)
+
+func GetPage(ctx context.Context, client *graphql.Client, slug string) (*gql.Page, error) {
+	if slug == "" {
+		return nil, fmt.Errorf("slug required to get page")
+	}
+	req := graphql.NewRequest(`
+query GetPage($slug: ID!) {
+	page(slug: $slug) {
+    slug
+    content
+    user {
+      id
+    }
+    meta {
+      key
+      value
+    }
+    modified
+	}
+}`)
+
+	req.Var("slug", slug)
+
+	var p *gql.Page
+	if err := client.Run(ctx, req, p); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func GetPages(ctx context.Context, client *graphql.Client) ([]*gql.Page, error) {
+	req := graphql.NewRequest(`
+query GetPages {
+	page(input: {
+    limit: 1000
+  }) {
+    slug
+    content
+    user {
+      id
+    }
+    meta {
+      key
+      value
+    }
+    modified
+	}
+}`)
+
+	var p []*gql.Page
+	if err := client.Run(ctx, req, p); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
