c587dacedfc32f6f839bfd135c407ecf13795ced
Nat Welch <nat@natwelch.com>
2023-01-17 15:42:31 +0000

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-#! /bin/sh
+#! /bin/zsh -ex
 
 git pull
 go get -v -d -u ./...
 go mod tidy -compat=1.19
-go build .
-git add go.mod go.sum && git ci -m 'chore: go update'
+git diff --quiet HEAD go.* || git add go.mod go.sum && git ci -m 'chore: go update'
 git push -u
+
+GIT_COMMIT=$(git rev-list -1 HEAD)
+go build -ldflags "-X main.CommitSHA=$GIT_COMMIT" .
 mv -v etu ~/bin/
